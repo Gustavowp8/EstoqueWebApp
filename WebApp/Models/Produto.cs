ae1fa3d1c1e8b3e16285619a4500debe8be3c152
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2023-10-03 17:10:22 -0300

    Adicionar arquivos de projeto.

---
+++ WebApp/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApp.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public double Preco { get; set; }
+
+        public double ValorEstoqueTotal()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
